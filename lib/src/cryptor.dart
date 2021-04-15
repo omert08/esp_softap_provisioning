91ea8ab0b719fc2ae8e308251a60d2cd81ba4eca
omert08 <tabanyazilim@gmail.com>
2021-04-15 15:56:27 +0300

    Cryptor class edited regarding to the native cipher code

---
+++ lib/src/cryptor.dart
@@ -0,0 +1,21 @@
+import 'dart:async';
+import 'dart:typed_data';
+import 'package:encrypt/encrypt.dart';
+import 'package:flutter/services.dart';
+
+class Cryptor {
+  static const MethodChannel _channel =
+  const MethodChannel('esp_softap_provisioning');
+
+  Future<bool> init (Uint8List key, Uint8List iv) async {
+    return await _channel.invokeMethod('init', {
+      'key': key,
+      'iv': iv,
+    });
+  }
+  Future<Uint8List> crypt (Uint8List data) async {
+    return _channel.invokeMethod('crypt', {
+      'data': data,
+    });
+  }
+}
