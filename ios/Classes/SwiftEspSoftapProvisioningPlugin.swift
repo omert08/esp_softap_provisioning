a2f276dec0580809979bc09c7574dff33ec6a742
omert08 <tabanyazilim@gmail.com>
2021-04-15 15:59:43 +0300

    Native cipher code for iOS

--- ios/Classes/SwiftEspSoftapProvisioningPlugin.swift
+++ ios/Classes/SwiftEspSoftapProvisioningPlugin.swift
@@ -2,6 +2,7 @@ import Flutter
 import UIKit
 
 public class SwiftEspSoftapProvisioningPlugin: NSObject, FlutterPlugin {
+  private var cryptoAES: CryptoAES?
   public static func register(with registrar: FlutterPluginRegistrar) {
     let channel = FlutterMethodChannel(name: "esp_softap_provisioning", binaryMessenger: registrar.messenger())
     let instance = SwiftEspSoftapProvisioningPlugin()
@@ -9,6 +10,24 @@ public class SwiftEspSoftapProvisioningPlugin: NSObject, FlutterPlugin {
   }
 
   public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
+
+    if call.method == "init" {
+        let args = call.arguments as! NSDictionary
+        let key = args["key"] as! FlutterStandardTypedData
+        let iv = args["iv"] as! FlutterStandardTypedData
+        cryptoAES = CryptoAES(key: key.data, iv: iv.data)
+        result(true)
+    } else if call.method == "crypt" {
+        let args = call.arguments as! NSDictionary
+        let data = args["data"] as! FlutterStandardTypedData
+        guard let cryptoAES = self.cryptoAES else {
+            return
+        }
+        let ret = cryptoAES.encrypt(data: data.data)
+
+        result(FlutterStandardTypedData(bytes: ret!))
+    }
     result("iOS " + UIDevice.current.systemVersion)
   }
+
 }
